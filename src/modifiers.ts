370f65e63dfc46dcf5ab02cfab0871a3fad43639
Iwata Hidetaka <iwata0303@gmail.com>
2018-03-09 08:36:58 +0900

    add modifier methods

---
+++ src/modifiers.ts
@@ -0,0 +1,23 @@
+const errmsg =
+  "[vue-jsx-modifier] this method should be removed by babel.\n" +
+  "Put 'babel-plugin-vue-jsx-modifier' into your babel configuration (before 'babel-plugin-transform-vue-jsx').";
+
+export function __sync<T>(_: T): T {
+  throw new Error(errmsg);
+}
+
+export function __capture<T extends (...args: any[]) => any>(_: T): T {
+  throw new Error(errmsg);
+}
+
+export function __once<T extends (...args: any[]) => any>(_: T): T {
+  throw new Error(errmsg);
+}
+
+export function __passive<T extends (...args: any[]) => any>(_: T): T {
+  throw new Error(errmsg);
+}
+
+export function __captureOne<T extends (...args: any[]) => any>(_: T): T {
+  throw new Error(errmsg);
+}
